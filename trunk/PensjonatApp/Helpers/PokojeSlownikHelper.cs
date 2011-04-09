cb1ae8aba065c35fd89bfadbbaeafb233fd4f947
dawid.myslak@gmail.com <dawid.myslak@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-04-09 21:12:47 +0000

    Wgrywam ostatni plik modelu powiazany z cala sprawa pokoi i wyposazen. Wszystko niezbedne do dodawania i edycji jest, banalnie uzyc tych metod modelu, w razie pytan pisac :P.

---
+++ trunk/PensjonatApp/Helpers/PokojeSlownikHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PensjonatApp.DS;
+using PensjonatApp.Tools;
+
+namespace PensjonatApp.Helpers
+{
+    class PokojeSlownikHelper
+    {
+        public static int dodajKlasePokoi(decimal cena, string dodatkowy_opis, int ilosc_osob, int[] tablicaIdElementowWyposazenia)
+        {
+            TablesManager.Manager.Pokoje_slownikTableAdapter.Insert(cena, dodatkowy_opis, ilosc_osob);
+            int? rekordZMaxID = TablesManager.Manager.Pokoje_slownikTableAdapter.GetDataMaxID();
+            for (int i = 0; i < tablicaIdElementowWyposazenia.Length; i++)
+                WyposazeniaHelper.dodajWyposazenie((int)rekordZMaxID, tablicaIdElementowWyposazenia[i]);
+            return 1;
+        }
+
+        public static int edytujKlasePokoi(int id_slownikowe_pokoju, decimal cena, string dodatkowy_opis, int ilosc_osob, int[] tablicaIdElementowWyposazenia)
+        {
+            PokojeDS.Pokoje_slownikDataTable doZmiany = TablesManager.Manager.Pokoje_slownikTableAdapter.GetDataByID(id_slownikowe_pokoju);
+            doZmiany[0].cena = cena;
+            doZmiany[0].dodatkowy_opis = dodatkowy_opis;
+            doZmiany[0].ilosc_osob = ilosc_osob;
+            TablesManager.Manager.Pokoje_slownikTableAdapter.Update(doZmiany);
+            TablesManager.Manager.WyposazeniaTableAdapter.DeleteData(id_slownikowe_pokoju);
+            for (int i = 0; i < tablicaIdElementowWyposazenia.Length; i++)
+                WyposazeniaHelper.dodajWyposazenie(id_slownikowe_pokoju, tablicaIdElementowWyposazenia[i]);
+            return 1;
+        }
+    }
+}
