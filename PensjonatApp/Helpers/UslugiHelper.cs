cb5f2baf852cc4f3f914e3525fd15b36087958c2
getoxs <getoxs@gmail.com>
2011-04-10 16:35:57 +0000

    Dodane nowe metody i poprawiono stare metody do Helpers/UslugiHelper.cs Dodano obsługę zapytań do słownika z typem usług w Helpers/UslugiSlownikHelper.cs

--- PensjonatApp/Helpers/UslugiHelper.cs
+++ PensjonatApp/Helpers/UslugiHelper.cs
@@ -10,18 +10,41 @@ namespace PensjonatApp.Helpers
 {
     class UslugiHelper
     {
-        public static int dodajUsluge(int id_pobytu, DateTime termin, string d_opis, int id_slownikowe)
+		/// <summary>
+		/// dodaje uslugę o danym typie, terminie do danego pobytu
+		/// </summary>
+        public static int dodajUsluge(int id_pobytu, DateTime termin, string d_opis, int? id_slownikowe)
         {
-
             return TablesManager.Manager.UslugiTableAdapter.Insert(id_pobytu, null, d_opis, termin, id_slownikowe);
-
         }
-
-        public static int przydzielPracownika(int id_uslugi, int id_pracownika)
+		/// <summary>
+		/// edytuje w usludze o danym id: typ, termin i id pobytu
+		/// </summary>
+		public static int edytujUsluge(int id_uslugi, int? id_pobytu, DateTime termin, string d_opis, int? id_slownikowe)
+		{
+			return TablesManager.Manager.UslugiTableAdapter.UpdateQuery(id_pobytu, d_opis, termin, id_slownikowe, id_uslugi);
+		}
+		/// <summary>
+		/// usuwa uslugę o danym id
+		/// </summary>
+		public static int usunUsluge(int id_uslugi)
+		{
+			return TablesManager.Manager.UslugiTableAdapter.DeleteById(id_uslugi);
+		}
+		//przydziały pracownicze
+		/// <summary>
+		/// edytuje w usludze o danym id: id_pracownika
+		/// </summary>
+        public static int przydzielPracownika(int id_uslugi, int? id_pracownika)
         {
-            UslugiDS.UslugiDataTable tab= TablesManager.Manager.UslugiTableAdapter.GetDataUslugiByID(id_uslugi);
-            tab[0].id_pracownika=id_pracownika;
-            return TablesManager.Manager.UslugiTableAdapter.Update(tab);
+			return TablesManager.Manager.UslugiTableAdapter.UpdateIdPracownika(id_pracownika, id_uslugi);
         }
+		/// <summary>
+		/// ustawia w usludze o danym id: id_pracownika na null
+		/// </summary>
+		public static int usunPracownika(int id_uslugi)
+		{
+			return przydzielPracownika(id_uslugi, null);
+		}
     }
 }
