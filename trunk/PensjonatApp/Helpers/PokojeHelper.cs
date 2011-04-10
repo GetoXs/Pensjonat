ac8c364c5c23902ce0b0a2d65a79bcaa2559dd48
dawid.myslak@gmail.com <dawid.myslak@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-04-10 17:36:09 +0000

    Wszystkie metody modeli zostaly zakomentowane.

--- trunk/PensjonatApp/Helpers/PokojeHelper.cs
+++ trunk/PensjonatApp/Helpers/PokojeHelper.cs
@@ -9,11 +9,19 @@ namespace PensjonatApp.Helpers
 {
     class PokojeHelper
     {
+        /// <summary>
+        /// Dodaje pokoj.
+        /// Wymagane parametry to id_slownikowe_pokoju wskazujace klase/kategorie pokoju oraz nr_pokoju.
+        /// </summary>
         public static int dodajPokoj(int id_slownikowe_pokoju, string nr_pokoju)
         {
             return TablesManager.Manager.PokojeTableAdapter.Insert(id_slownikowe_pokoju, nr_pokoju);
         }
 
+        /// <summary>
+        /// Edytuje pokoj.
+        /// Wymagane parametry to id_slownikowe_pokoju wskazujace klase/kategorie pokoju, nr_pokoju oraz ID edytowanego pokoju.
+        /// </summary>
         public static int edytujPokoj(int id_pokoju, int id_slownikowe_pokoju, string nr_pokoju)
         {
             PokojeDS.PokojeDataTable doZmiany = TablesManager.Manager.PokojeTableAdapter.GetDataByID(id_pokoju);
--- trunk/PensjonatApp/Helpers/WyposazeniaHelper.cs
+++ trunk/PensjonatApp/Helpers/WyposazeniaHelper.cs
@@ -9,6 +9,10 @@ namespace PensjonatApp.Helpers
 {
     class WyposazeniaHelper
     {
+        /// <summary>
+        /// Dodaje wyposazenie (powiazanie miedzy kategoria pokoju i slownikiem wyposazenia).
+        /// Prosze nie wywolywac jej z poziomu interfejsu.
+        /// </summary>
         public static int dodajWyposazenie(int id_slownikowe_pokoju, int id_wyposazenia)
         {
             return TablesManager.Manager.WyposazeniaTableAdapter.Insert(id_slownikowe_pokoju, id_wyposazenia);
