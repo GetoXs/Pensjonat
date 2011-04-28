b8fc9bfc227b8b36cf56d37bad8e9295967d2ce1
getoxs@gmail.com <getoxs@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-04-28 19:40:41 +0000

    Dodanie helpera dla Posiłków, i ich słownika Zaktualizowanie bazy danych o tablę Rabatów (wcześniej nie było żadnej) Dodanie helpera Rabatów Dodanie do RachunkiDS tabeli z Rabatami Dodanie testowej funkcjonalności wypełniania listy ComboBoxów, a mianowicie, po naciśnięciu "Dodaj Rabat" w kategorii z rozliczeniami Pobytu, wypełnia nam się lista aktualnych Rabatów, do skasowania w późniejszych implementacjach.

---
+++ trunk/PensjonatApp/Helpers/PosilkiHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PensjonatApp.DS;
+using PensjonatApp.Tools;
+
+namespace PensjonatApp.Helpers
+{
+    class PosilkiHelper
+    {
+        public static int dodajPosilek(int? id_pobytu, int? id_slownikowe_posilku, DateTime? data)
+        {
+            return TablesManager.Manager.PosilkiTableAdapter.Insert(id_pobytu, id_slownikowe_posilku, data);
+        }
+        public static int edytujPosilek(int id_posilku, int? id_pobytu, int? id_slownikowe_posilku, DateTime? data)
+        {
+            return TablesManager.Manager.PosilkiTableAdapter.UpdateQuery(id_pobytu, id_slownikowe_posilku, data, id_posilku);
+        }
+        public static int usunPosilek(int id_posilku)
+        {
+            return TablesManager.Manager.PosilkiTableAdapter.DeleteQueryById(id_posilku);
+        }
+    }
+}
---
+++ trunk/PensjonatApp/Helpers/RabatyHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PensjonatApp.DS;
+using PensjonatApp.Tools;
+
+namespace PensjonatApp.Helpers
+{
+    class RabatyHelper
+    {
+        public static int dodajRabat(string nazwa, bool procentowy, int? wartosc, bool aktywny)
+        {
+            return TablesManager.Manager.RabatyTableAdapter.Insert(nazwa, procentowy, wartosc, aktywny);
+        }
+        public static int edytujRabat(int Original_id_rabatu, string nazwa, bool procentowy, int? wartosc, bool aktywny)
+        {
+            return TablesManager.Manager.RabatyTableAdapter.UpdateQuery(nazwa, procentowy, wartosc, aktywny, Original_id_rabatu);
+        }
+        public static int kasujRabat(int id_rabatu)
+        {
+            return TablesManager.Manager.RabatyTableAdapter.DeleteQueryById(id_rabatu);
+        }
+    }
+}
