670f40c5923351123495c1e4774755330c4caa71
michal.lokaj@gmail.com <michal.lokaj@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-05-18 10:41:09 +0000

    Dodałem: PracownicyHelper -dodajPracownika -edytujPracownika -usunPracownika

---
+++ trunk/PensjonatApp/Helpers/PracownicyHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PensjonatApp.Tools;
+
+namespace PensjonatApp.Helpers
+{
+    class PracownicyHelper
+    {
+        /// <summary>
+        /// dodaje pracownika o zadanym stanowisku
+        /// </summary>
+        /// <param name="imie"></param>
+        /// <param name="nazwisko"></param>
+        /// <param name="login"></param>
+        /// <param name="haslo"></param>
+        /// <param name="id_p"></param>
+        /// <param name="id_s"></param>
+        /// <returns></returns>
+        public static int dodajPracownika(string imie, string nazwisko, string login, string haslo, int id_p, int? id_s)
+        {
+            return TablesManager.Manager.PracownicyTableAdapter.InsertQuery(id_s, imie, nazwisko, login, haslo);
+        }
+
+        public static int edytujPracownikaByIdPr(string imie, string nazwisko, string login, string haslo, int id_p, int? id_s)
+        {
+            return TablesManager.Manager.PracownicyTableAdapter.UpdateQuerybyIdPr(id_s, imie, nazwisko, login, haslo, id_p);
+        }
+
+        public static int usunPracownika(int id_p)
+        {
+            return TablesManager.Manager.PracownicyTableAdapter.DeleteQuery(id_p);
+        }
+    }
+}
