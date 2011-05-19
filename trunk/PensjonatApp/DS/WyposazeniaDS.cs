6227c410c3ddac9c47317d019bfa1c6325c4be25
mknura <mknura@b9b0d238-c329-33f3-7785-67001447cd33>
2011-05-19 22:36:22 +0000

    Interfejs

--- trunk/PensjonatApp/DS/WyposazeniaDS.cs
+++ trunk/PensjonatApp/DS/WyposazeniaDS.cs
@@ -13,3 +13,10 @@ namespace PensjonatApp.DS {
     public partial class WyposazeniaDS {
     }
 }
+
+namespace PensjonatApp.DS.WyposazeniaDSTableAdapters {
+    
+    
+    public partial class Wyposazenia_slownikTableAdapter {
+    }
+}
