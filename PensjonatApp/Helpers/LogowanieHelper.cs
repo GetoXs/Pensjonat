24723f9f4d23f76d24177ea08c420fa17aaa776b
dawid.myslak@gmail.com <dawid@myslak.me>
2011-05-21 13:44:49 +0000

    Poprawki zwiazane z logowaniem.

--- PensjonatApp/Helpers/LogowanieHelper.cs
+++ PensjonatApp/Helpers/LogowanieHelper.cs
@@ -14,19 +14,20 @@ namespace PensjonatApp.Helpers
         public static string Nazwisko;
         public static string Stanowisko;
 
-        public static void zalogujPracownika(string login, string haslo)
+        public static bool zalogujPracownika(string login, string haslo)
         {
             int id_stanowiska;
             PracownicyDS.PracownicyDataTable pracownik = TablesManager.Manager.PracownicyTableAdapter.GetPracownicyByLoginHaslo(login, haslo);
             if (pracownik.Count > 0)
                 id_stanowiska = pracownik[0].id_stanowiska;
             else
-                throw new Exception();
+                return false;
             PracownicyDS.Pracownicy_slownikDataTable stanowisko = TablesManager.Manager.Pracownicy_slownikTableAdapter.GetDataByID(id_stanowiska);
             ID = pracownik[0].id_pracownika;
             Imie = pracownik[0].imie;
             Nazwisko = pracownik[0].nazwisko;
             Stanowisko = stanowisko[0].nazwa;
+            return true;
         }
     }
 }
