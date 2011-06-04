6e9e257ab341b84d2fde6eebcbe76cb5e918f5a0
mknura <mknura@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-04 22:25:29 +0000

    Prace nad ekranem dodawania nowego pobytu

---
+++ trunk/PensjonatApp/Helpers/InterfaceHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PensjonatApp.Helpers
+{
+    class PokojeKlienci
+    {
+        string imie;
+        string nazwisko;
+        string pesel;
+        string nr_pokoju;
+
+        public string Imie { get { return imie; } set { imie = value; } }
+        public string Nazwisko { get { return nazwisko; } set { nazwisko = value; } }
+        public string PESEL { get { return pesel; } set { pesel = value; } }
+        public string Nr_pokoju { get { return nr_pokoju; } set { nr_pokoju = value; } }
+
+        public PokojeKlienci(string imie, string nazwisko, string PESEL, string nr_pokoju)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.PESEL = PESEL;
+            this.nr_pokoju = nr_pokoju;
+        }
+    }
+
+}
