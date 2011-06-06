ff1b325f01bfa8b598f578153d55b78e1c5459b2
michal.lokaj@gmail.com <michal.lokaj@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-06 19:08:31 +0000

    Dodano pobieranie posilkow po terminie w PosilkiHelper.cs

--- PensjonatApp/Helpers/PosilkiHelper.cs
+++ PensjonatApp/Helpers/PosilkiHelper.cs
@@ -21,5 +21,73 @@ namespace PensjonatApp.Helpers
         {
             return TablesManager.Manager.PosilkiTableAdapter.DeleteQueryById(id_posilku);
         }
+
+        public static SumaPosilkow getPosilkiPoTerminie(DateTime? data)
+        {
+            SumaPosilkow suma = new SumaPosilkow();
+
+            suma.Sniadania = TablesManager.Manager.PosilkiTableAdapter.GetSumSniadaniabyTermin(data).GetValueOrDefault(0);
+            suma.Dsniadania = TablesManager.Manager.PosilkiTableAdapter.GetSumDsniadania(data).GetValueOrDefault(0);
+            suma.Obiady = TablesManager.Manager.PosilkiTableAdapter.GetSumObiadyByTermin(data).GetValueOrDefault(0);
+            suma.Lunche = TablesManager.Manager.PosilkiTableAdapter.GetSumLunchbyTermin(data).GetValueOrDefault(0);
+            suma.Podwieczorki = TablesManager.Manager.PosilkiTableAdapter.GetSumPodw(data).GetValueOrDefault(0);
+            suma.Obiadokolacje = TablesManager.Manager.PosilkiTableAdapter.GetSumObiadokolacjeByTermin(data).GetValueOrDefault(0);
+            suma.Kolacje = TablesManager.Manager.PosilkiTableAdapter.GetSumKolacjeByTermin(data).GetValueOrDefault(0);
+
+            return suma;
+        }
+
+        public class SumaPosilkow
+        {
+            int sniadania;
+            public int Sniadania
+            {
+                get { return sniadania; }
+                set { sniadania = value; }
+            }
+            
+            int dsniadania;
+            public int Dsniadania
+            {
+                get { return dsniadania; }
+                set { dsniadania = value; }
+            }
+
+            int lunche;
+
+            public int Lunche
+            {
+                get { return lunche; }
+                set { lunche = value; }
+            }
+            int obiady;
+
+            public int Obiady
+            {
+                get { return obiady; }
+                set { obiady = value; }
+            }
+            int obiadokolacje;
+
+            public int Obiadokolacje
+            {
+                get { return obiadokolacje; }
+                set { obiadokolacje = value; }
+            }
+            int podwieczorki;
+
+            public int Podwieczorki
+            {
+                get { return podwieczorki; }
+                set { podwieczorki = value; }
+            }
+            int kolacje;
+
+            public int Kolacje
+            {
+                get { return kolacje; }
+                set { kolacje = value; }
+            }
+        }
     }
 }
