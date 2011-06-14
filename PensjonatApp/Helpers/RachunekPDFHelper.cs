bd65797fdf2ed329d4c5350db3e3534aa164de30
michal.lokaj@gmail.com <michal.lokaj@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-14 16:54:24 +0000

    PDF

--- PensjonatApp/Helpers/RachunekPDFHelper.cs
+++ PensjonatApp/Helpers/RachunekPDFHelper.cs
@@ -28,14 +28,31 @@ namespace PensjonatApp.Helpers
             naglowek.Add("Rachunek\n");
             Paragraph naglowek2 = new Paragraph();
             naglowek2.Add("Id rachunku: "+id_rachunku.ToString()+"\n");
-            naglowek2.Add(DateTime.Now.Date.ToString()+"\n");
+            naglowek2.Add(DateTime.Now.Date.ToString()+"\n\n");
 
             doc.Add(naglowek);
             doc.Add(naglowek2);
 
             PobytyDS.PobytyDataTable pobyty = TablesManager.Manager.PobytyTableAdapter.GetDataPobytyByIdRachunku(id_rachunku);
 
-            //foreach (PobytyDS.PobytyRow
+            foreach (PobytyDS.PobytyRow p in pobyty)
+            {
+                Paragraph par = new Paragraph();
+                KlienciDS.KlienciRow klient=TablesManager.Manager.KlienciTableAdapter.GetDataByIdKlienta(p.id_klienta)[0];
+                par.Add(klient.imie + " " + klient.nazwisko + "\n");
+                if (!(klient.nazwa.Equals("")))
+                    par.Add(klient.nazwa+"\n");
+                par.Add("Pokój: " + RozliczenieHelper.pobierzPodstawowaCenaPobytu(p.id_pobytu).ToString("0.00") + " PLN\n");
+                par.Add("Posiłki: " + RozliczenieHelper.pobierzPodstawowaCenaPosilkow(p.id_pobytu).ToString("0.00") + " PLN\n");
+                par.Add("Usługi: " + RozliczenieHelper.pobierzPodstawowaCenaUslug(p.id_pobytu).ToString("0.00") + " PLN\n\n");
+
+                doc.Add(par);
+            }
+
+            Paragraph doZaplaty = new Paragraph();
+            RachunkiDS.RachunkiRow r=TablesManager.Manager.RachunkiTableAdapter.GetDataById(id_rachunku)[0];
+            doZaplaty.Add("Do zapłacenia po uwzględnieniu rabatów i zaliczki: "+r.wartosc.ToString("0.00")+" PLN\n");
+            doc.Add(doZaplaty);
 
             doc.Close();
             return true;
