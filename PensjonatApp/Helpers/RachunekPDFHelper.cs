1146e77cde91d05703c809520ec538e00c46d6ea
michal.lokaj@gmail.com <michal.lokaj@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-18 21:18:32 +0000

    PDF

--- PensjonatApp/Helpers/RachunekPDFHelper.cs
+++ PensjonatApp/Helpers/RachunekPDFHelper.cs
@@ -27,9 +27,9 @@ namespace PensjonatApp.Helpers
             FontFactory.RegisterDirectory("C:\\WINDOWS\\Fonts");
 
             Font duzy = FontFactory.GetFont("Verdana",BaseFont.CP1250,16);
-            Font sredni = FontFactory.GetFont("Verdana", BaseFont.CP1250, 12);
-            Font sredniB = FontFactory.GetFont("Verdana", BaseFont.CP1250, 12);
-            Font maly = FontFactory.GetFont("Verdana", BaseFont.CP1250, 10);
+            Font sredni = FontFactory.GetFont("Verdana", BaseFont.CP1250, 10);
+            Font sredniB = FontFactory.GetFont("Verdana", BaseFont.CP1250, 10);
+            Font maly = FontFactory.GetFont("Verdana", BaseFont.CP1250, 9);
             duzy.SetStyle(Font.BOLD);
             sredniB.SetStyle(Font.BOLD);
 
@@ -40,15 +40,18 @@ namespace PensjonatApp.Helpers
 
             Paragraph naglowek2 = new Paragraph("",maly);
             naglowek2.Add("Id rachunku: "+id_rachunku.ToString()+"\n");
-            naglowek2.Add("Data: "+DateTime.Now.Date.ToString("d-MM-yyyy")+"\n\n");
+            naglowek2.Add("Data wygenerowania: "+DateTime.Now.Date.ToString("d-MM-yyyy")+"\n\n");
             doc.Add(naglowek2);
 
             PobytyDS.PobytyDataTable pobyty = TablesManager.Manager.PobytyTableAdapter.GetDataPobytyByIdRachunku(id_rachunku);
 
-            PdfPTable tabela = new PdfPTable(5);
+            PdfPTable tabela = new PdfPTable(7);
             tabela.SpacingBefore=40;
             tabela.SpacingAfter=40;
             tabela.AddCell(new Phrase("Klient:",sredniB));
+            //tabela.AddCell(new Phrase("PESEL:", sredniB));
+            tabela.AddCell(new Phrase("Początek pobytu:", sredniB));
+            tabela.AddCell(new Phrase("Koniec pobytu:", sredniB));
             tabela.AddCell(new Phrase("Opłata za pokój:",sredniB));
             tabela.AddCell(new Phrase("Opłata za posiłki:",sredniB));
             tabela.AddCell(new Phrase("Opłata za usługi:",sredniB));
@@ -62,6 +65,9 @@ namespace PensjonatApp.Helpers
                 decimal suma = 0;
                 decimal koszt = 0;
                 tabela.AddCell(new Phrase(klient.imie + " " + klient.nazwisko,sredniB));
+                //tabela.AddCell(new Phrase(klient.pesel, sredniB));
+                tabela.AddCell(new Phrase(p.termin_start.Date.ToString("d-MM-yyyy"), sredni));
+                tabela.AddCell(new Phrase(p.termin_koniec.Date.ToString("d-MM-yyyy"), sredni));
                
                 koszt = RozliczenieHelper.pobierzPodstawowaCenaPobytu(p.id_pobytu);
                 suma += koszt;
