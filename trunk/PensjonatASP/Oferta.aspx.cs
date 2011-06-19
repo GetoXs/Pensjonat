e354f2bcdb00e42fcc472f5166dca0effd9e0aa9
tomekc4@gmail.com <tomekc4@gmail.com@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-19 18:02:20 +0000

    Nowa wersja helpa i asp.

--- trunk/PensjonatASP/Oferta.aspx.cs
+++ trunk/PensjonatASP/Oferta.aspx.cs
@@ -29,118 +29,141 @@ namespace PensjonatASP
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Db.Con.Open();
-            DbCommand cmd = Db.Con.CreateCommand();
-
-            bool blad = false;
-            GridView1.Visible = false;
-
-            String start = Calendar1.SelectedDate.ToString("#MM/dd/yy#");
-            String koniec = Calendar2.SelectedDate.ToString("#MM/dd/yy#");
-           
-            if (Calendar1.SelectedDate >= Calendar2.SelectedDate || Calendar1.SelectedDate < Calendar1.TodaysDate)
-            {
-                blad = true;
-                Label1.Text = "Nieprawidłowy termin pobytu.";
-            }
-
             try
             {
-                ile = Int32.Parse(TextBox1.Text);
-            }
-            catch (Exception ex)
-            {
-                blad = true;
-                Label1.Text = "Błędna ilość osób.";
-            }
+                bool blad = false;
+                GridView1.Visible = false;
+           
+                Db.Con.Open();
+                DbCommand cmd = Db.Con.CreateCommand();
 
-            if (ile <= 0)
-            {
-                blad = true;
-                Label1.Text = "Błędna ilość osób.";
-            }
+                String start = Calendar1.SelectedDate.ToString("#MM/dd/yy#");
+                String koniec = Calendar2.SelectedDate.ToString("#MM/dd/yy#");
 
-
-            if (!blad)
-            {
-                cmd.CommandText = "SELECT        p3.id_pokoju AS ID, p3.nr_pokoju AS NUMER, p4.cena AS CENA, p4.dodatkowy_opis AS OPIS " +
-                                    "FROM            Pokoje p3, Pokoje_slownik p4 " +
-                                    "WHERE        p3.id_slownikowe_pokoju = p4.id_slownikowe_pokoju AND p4.ilosc_osob = " + ile + " AND (p3.id_pokoju NOT IN " +
-                                    "(SELECT        id_pokoju " +
-                                    "FROM            Pobyty p1 " +
-                                    "WHERE        (termin_start <= " + start + ") AND (termin_koniec >= " + start + ") OR " +
-                                                             "(termin_start <= " + koniec + ") AND (termin_koniec >= " + koniec + ") OR " +
-                                                             "(termin_start >= " + start + ") AND (termin_koniec <= " + koniec + ")))";
-
-                //Label1.Text = cmd.CommandText;
-                DbDataReader reader = cmd.ExecuteReader();
-
-                if (reader.HasRows)
+                if (Calendar1.SelectedDate >= Calendar2.SelectedDate || Calendar1.SelectedDate < Calendar1.TodaysDate)
                 {
-                    GridView1.DataSource = reader;
-                    GridView1.DataBind();
-                    Label1.Text = "Dostępne pokoje: ";
-                    GridView1.Visible = true;
-                    //Button3.Visible = true;
-                    //Button4.Visible = true;
-                    
-                    
-                }
-                else
-                {
-                    Label1.Text = "Brak dostępnych pokojów.";
-                    GridView1.Visible = false;
-                    //Button3.Visible = false;
-                    //Button4.Visible = false;
+                    blad = true;
+                    Label1.Text = "Nieprawidłowy termin pobytu.";
                 }
 
-                Db.Con.Close();
+                try
+                {
+                    ile = Int32.Parse(TextBox1.Text);
+                }
+                catch (Exception ex)
+                {
+                    blad = true;
+                    Label1.Text = "Błędna ilość osób.";
+                }
+
+                if (ile <= 0)
+                {
+                    blad = true;
+                    Label1.Text = "Błędna ilość osób.";
+                }
+
+
+                if (!blad)
+                {
+                    cmd.CommandText = "SELECT        p3.id_pokoju AS ID, p3.nr_pokoju AS NUMER, p4.cena AS CENA, p4.dodatkowy_opis AS OPIS " +
+                                        "FROM            Pokoje p3, Pokoje_slownik p4 " +
+                                        "WHERE        p3.id_slownikowe_pokoju = p4.id_slownikowe_pokoju AND p4.ilosc_osob = " + ile + " AND (p3.id_pokoju NOT IN " +
+                                        "(SELECT        id_pokoju " +
+                                        "FROM            Pobyty p1 " +
+                                        "WHERE        (termin_start <= " + start + ") AND (termin_koniec >= " + start + ") OR " +
+                                                                 "(termin_start <= " + koniec + ") AND (termin_koniec >= " + koniec + ") OR " +
+                                                                 "(termin_start >= " + start + ") AND (termin_koniec <= " + koniec + ")))";
+
+                    //Label1.Text = cmd.CommandText;
+                    DbDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        GridView1.DataSource = reader;
+                        GridView1.DataBind();
+                        Label1.Text = "Dostępne pokoje: ";
+                        GridView1.Visible = true;
+                        //Button3.Visible = true;
+                        //Button4.Visible = true;
+
+
+                    }
+                    else
+                    {
+                        Label1.Text = "Brak dostępnych pokojów.";
+                        GridView1.Visible = false;
+                        //Button3.Visible = false;
+                        //Button4.Visible = false;
+                    }
+
+                    Db.Con.Close();
+                }
+
+
+            }catch(Exception ex)
+            {
+                
+                Label1.Text = "Błąd połączenia z bazą danych.";
             }
+            
+           
 
         }
 
          
         protected void Button2_Click(object sender, EventArgs e)
         {
-            Db.Con.Open(); 
-            DbCommand cmd = Db.Con.CreateCommand();
-
-            bool blad = false;
-            pesel = TextBox3.Text;
+            
 
             try
             {
-                id_rez = Int32.Parse(TextBox2.Text);
-            }
-            catch (Exception ex)
-            {
-                blad = true;
-                Label2.Text = "Nie ma takiej rezerwacji, bądź nieprawidłowe dane!";
-            }
+                bool blad = false;
+                Db.Con.Open();
+                DbCommand cmd = Db.Con.CreateCommand();
 
-            if (id_rez <= 0)
-            {
-                blad = true;
-                Label2.Text = "Nie ma takiej rezerwacji, bądź nieprawidłowe dane!";
-            }
+                pesel = TextBox3.Text;
 
-            if (!blad)
-            {
-                cmd.CommandText = "SELECT r1.zaplacono_zaliczke, r1.zaliczka FROM Rezerwacje r1, Klienci k1 WHERE r1.id_klienta = k1.id_klienta AND r1.id_rezerwacji = " + id_rez + " AND k1.pesel LIKE '"+ pesel +"'";
-                DbDataReader reader = cmd.ExecuteReader();
-                
-                if (reader.HasRows)
+                try
                 {
-                    reader.Read();
-                    Label2.Text = "Zaliczka: " + reader.GetFloat(1).ToString() + "zł - " + (reader.GetBoolean(0) ? "wpłacono" : "nie wpłacono");
+                    id_rez = Int32.Parse(TextBox2.Text);
                 }
-                else
+                catch (Exception ex)
                 {
+                    blad = true;
                     Label2.Text = "Nie ma takiej rezerwacji, bądź nieprawidłowe dane!";
                 }
 
-                Db.Con.Close();
+                if (id_rez <= 0)
+                {
+                    blad = true;
+                    Label2.Text = "Nie ma takiej rezerwacji, bądź nieprawidłowe dane!";
+                }
+
+                if (!blad)
+                {
+                    cmd.CommandText = "SELECT r1.zaplacono_zaliczke, r1.zaliczka FROM Rezerwacje r1, Klienci k1 WHERE r1.id_klienta = k1.id_klienta AND r1.id_rezerwacji = " + id_rez + " AND k1.pesel LIKE '" + pesel + "'";
+                    DbDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        Label2.Text = "Zaliczka: " + reader.GetFloat(1).ToString() + "zł - " + (reader.GetBoolean(0) ? "wpłacono" : "nie wpłacono");
+                    }
+                    else
+                    {
+                        Label2.Text = "Nie ma takiej rezerwacji, bądź nieprawidłowe dane!";
+                    }
+
+                    Db.Con.Close();
+                }
+
             }
+            catch (Exception ex)
+            {
+                Label2.Text = "Błąd połączenia z bazą danych.";
+            }
+            
+            
         }
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
