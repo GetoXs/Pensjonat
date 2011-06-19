c0223c737dee00d4bd91b27eb2e75eef2788ee73
getoxs <getoxs@b9b0d238-c329-33f3-7785-67001447cd33>
2011-06-19 21:19:07 +0000

    Dodano walidacje danych dla dodawania oraz edytowania pracownika Do testów. Duplikacja rozwiązań jak dla dodawania klienta Ujednolicenie nazewnictwa, oraz osobny katalog dla plików związanych z walidacją:  - Validation/ValidationHelper.cs - funkcje pomocnicze do walidacji (ogólne funkcje wymuszania walidacji, oraz sprawdzenia czy są poprawne dane),  - Validation/ValidationRules.cs - Klasy zasady zawierające warunki dla jakich dana zasada ma być akceptowana,  - Validation/ValidationKlient.cs, ValidationPracownik.cs - pliki z danymi oraz specyficznymi wymuszeniami walidacji czy też sprawdzania poprawności dla odpowiednio klienta i pracownika (zarówno dodawania nowego, jak i edycji)
    Poprawki:
     - usunieto z dodawania oraz edycji pracownika pola PESELu oraz telefonu (i tak ich odwzorowania nie ma w bazie)

---
+++ trunk/PensjonatApp/Helpers/Validation/ValidationHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using PensjonatApp;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace PensjonatApp.ValidationEx
+{
+	class ValidationHelper
+	{
+		public static bool isValid(DependencyObject parent)
+		{
+			if (Validation.GetHasError(parent))
+				return false;
+
+			DependencyObject child;
+			// Validate all the bindings on the children
+			for (int i = 0; i != VisualTreeHelper.GetChildrenCount(parent); ++i)
+			{
+				child = VisualTreeHelper.GetChild(parent, i);
+				if (!isValid(child)) { return false; }
+			}
+
+			return true;
+		}
+		public static void forceUpdate(DependencyObject parent)
+		{
+			DependencyObject child;
+			System.Windows.Data.BindingExpression bind;
+			for (int i = 0; i != VisualTreeHelper.GetChildrenCount(parent); ++i)
+			{
+				child = VisualTreeHelper.GetChild(parent, i);
+				if (child is FrameworkElement)
+				{
+					bind = ((FrameworkElement)child).GetBindingExpression(TextBox.TextProperty);
+					if (bind != null)
+						bind.UpdateSource();
+				}
+				forceUpdate(child);
+			}
+		}
+	}
+}
---
+++ trunk/PensjonatApp/Helpers/Validation/ValidationPracownik.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using PensjonatApp.ValidationEx;
+
+namespace PensjonatApp
+{
+	public class ValidationPracownik
+	{
+		private string _imie;
+		private string _nazwisko;
+		private string _login;
+		private string _pesel;
+		private string _telefon;
+		private string _stanowisko;
+
+		#region Getters&Setters
+		public string Imie
+		{
+			get { return _imie; }
+			set { _imie = value; }
+		}
+		public string Nazwisko
+		{
+			get { return _nazwisko; }
+			set { _nazwisko = value; }
+		}
+		public string Login
+		{
+			get { return _login; }
+			set { _login = value; }
+		}
+		public string Pesel
+		{
+			get { return _pesel; }
+			set { _pesel = value; }
+		}
+		public string Telefon
+		{
+			get { return _telefon; }
+			set { _telefon = value; }
+		}
+		public string Stanowisko
+		{
+			get { return _stanowisko; }
+			set { _stanowisko = value; }
+		}
+		#endregion
+		/// <summary>
+		/// Sprawdza czy wszystkie wymagane pola sa wypelnione
+		/// </summary>
+		public static bool isValid(DependencyObject parent)
+		{
+			ValidationHelper.forceUpdate(parent);
+			return ValidationHelper.isValid(parent);
+		}
+	}
+}
